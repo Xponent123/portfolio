ad05f8d117822932c776ff6af4c5c5b37ce4a1bf
Xponent321 <jal.parikh@students.iiit.ac.in>
2025-05-15 03:18:13 +0530

    Fix routing for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,22 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import { useTheme } from "./context/ThemeContext.jsx";
-
 import { Footer, Navbar } from "./components";
 import { About, Contact, Home, Projects } from "./pages";
 
 const App = () => {
   const { isDarkMode } = useTheme();
-  
+
   return (
     <main className={`${isDarkMode ? 'bg-gray-900 text-white' : 'bg-slate-300/20 text-black'} min-h-screen transition-colors duration-300 pt-16`}>
-      <Router>
+      <Router basename="/portfolio">
         <Navbar />
         <Routes>
           <Route path='/' element={<Home />} />
-          <Route
-            path='/*'
-            element={
-              <>
-                <Routes>
-                  <Route path='/about' element={<About />} />
-                  <Route path='/projects' element={<Projects />} />
-                  <Route path='/contact' element={<Contact />} />
-                </Routes>
-                <Footer />
-              </>
-            }
-          />
+          <Route path='/about' element={<About />} />
+          <Route path='/projects' element={<Projects />} />
+          <Route path='/contact' element={<Contact />} />
         </Routes>
+        <Footer />
       </Router>
     </main>
   );
